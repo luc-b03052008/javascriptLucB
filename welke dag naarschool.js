e1a25bf60ddf9004dac9a7d61e036ce09d0beebb
luc bruggeman <38404@ma-web.com>
2024-10-02 17:04:16 +0200

    opdracht 2

---
+++ welke dag naarschool.js
@@ -0,0 +1,16 @@
+const vandaag = new Date()
+const huidigedag = vandaag.getDate(); 
+
+if (huidigedag == 0){
+    console.log("het is zondag");
+} else if (huidigedag === 6){
+    console.log("het is zaterdag")
+}else{
+    console.log("naar school")
+}
+
+
+
+
+    
+
